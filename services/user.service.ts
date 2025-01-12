c922ca3e34294298390fbed161c53ae46cbb5a1c
MandemGibson <themaingib@gmail.com>
2025-01-12 23:20:05 +0000

    feat(user service): add getAllUsers function to retrieve all users

--- services/user.service.ts
+++ services/user.service.ts
@@ -13,3 +13,11 @@ export const addUser = async (payload: {
     console.error("Error adding a user: ", error.message);
   }
 };
+
+export const getAllUsers = async () => {
+  try {
+    return await prisma.user.findMany()
+  } catch (error: any) {
+    console.error("Error getting all users: ", error.message)
+  }
+}
