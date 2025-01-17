b6c6b4a7b4b54951e85f63ed2a2e7a0fdd96fb73
MandemGibson <themaingib@gmail.com>
2025-01-17 22:27:07 +0000

    token: generate token

---
+++ utils/token.ts
@@ -0,0 +1,29 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const generateToken = async (
+  userId: string,
+  res: Response,
+  rememberMe: boolean
+) => {
+  try {
+    const token = await jwt.sign(
+      { userId },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "2d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 2 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+  } catch (error: any) {
+    console.error("Error generating token: ", error.message);
+  }
+};
+
+
