638c58d7a015c4f00716c0a47997993c0ee97b62
MandemGibson <themaingib@gmail.com>
2025-01-19 22:17:39 +0000

    setup: add middleware for cookie parsing, logging, and environment configuration

--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import { config } from "dotenv"
+import morgan from "morgan"
+import cookieParser from "cookie-parser"
+
+config()
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
